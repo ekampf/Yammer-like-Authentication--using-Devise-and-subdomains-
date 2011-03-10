f76d65ebb2d13b99b3c94a1b5d48143878b56912
Eran Kampf <eran@ekampf.com>
2011-03-10 18:07:17 +0200

    ActionMailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,14 @@ YammerLikeAuth::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
+  ### ActionMailer Config
+  # A dummy setup for development - no deliveries, but logged
+  config.action_mailer.default_url_options = { :host => 'local.host:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
