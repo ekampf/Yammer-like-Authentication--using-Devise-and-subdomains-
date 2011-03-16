87085e1d6d6664caeef515a9902d6835fc32a751
Eran Kampf <eran@ekampf.com>
2011-03-16 17:19:44 +0200

    Prevent signups from subdomains

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_messages
+    result =""
+      %w(notice warning error alert).each do |msg|
+        result <<  content_tag(:div, content_tag(:p, flash[msg.to_sym]),
+          :class => "message #{msg}") unless flash[msg.to_sym].blank?
+      end
+      result.html_safe
+  end
 end
